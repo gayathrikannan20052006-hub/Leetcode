fa98559dc88e7f720894ac30fbe654ec3295275f
Gayathri K <gayathrikannan20052006@gmail.com>
2026-02-24 00:54:19 +0530

    Implement function to find length of last word

---
+++ lengthOfLastWord.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n=s.length();
+        int len=0;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                len++;
+            }
+            else if(len>0){
+                break;
+            }
+        }
+    return len;
+    }
+}
