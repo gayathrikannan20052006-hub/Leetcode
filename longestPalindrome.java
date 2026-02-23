eb9ba72f17be9b48dc6569b3547a9a0a37b294a7
Gayathri K <gayathrikannan20052006@gmail.com>
2026-02-24 01:01:22 +0530

    Create longestPalindrome.java

---
+++ longestPalindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestPalindrome(String s) {
+     String longest="";
+     int n=s.length();
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(ispalin(s,i,j)){
+                if(longest.length()<j-i+1){
+                    longest=s.substring(i,j+1);
+                }
+            }
+        }
+     }return longest;
+
+    }
+    public static boolean ispalin(String str,int left,int right){
+       while(left<right){
+        if(str.charAt(left)!=str.charAt(right)){
+            return false;
+        }
+        left++;right--;
+       }return true;
+    }
+}
