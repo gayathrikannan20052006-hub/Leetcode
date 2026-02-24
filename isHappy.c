027bdb44123b4731426172bdc461b47dad76d7e0
Gayathri K <gayathrikannan20052006@gmail.com>
2026-02-24 06:50:38 +0530

    Implement isHappy function to check happy numbers

---
+++ isHappy.c
@@ -0,0 +1,17 @@
+bool isHappy(int n) {
+    if(n==1||n==7){
+        return true;
+    }
+    else if(n<10){
+        return false;
+    }
+    else{
+        int sum=0;
+        while(n>0){
+            int r=n%10;
+            sum= sum+r*r;
+            n=n/10;
+        }
+        return isHappy(sum);
+    }
+}
